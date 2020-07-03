6cc56728c09c12499c6d8977550b4371efbe138b
Tashona Smith <tashona.smith@gmail.com>
2020-07-03 12:31:16 -0500

    Create article model

---
+++ models/Article.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    summary: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    comment: {
+      type: Schema.Types.ObjectId,
+      ref: "Note"
+    }
+  });
+  
+  var Article = mongoose.model("Article", ArticleSchema);
+  
+  module.exports = Article;
+  
