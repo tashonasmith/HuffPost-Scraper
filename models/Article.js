20e12d65aff3fe50316e19b7f37e0da42947687f
Tashona Smith <tashona.smith@gmail.com>
2020-07-05 11:58:28 -0500

    Change to HuffPost.  Write GET route for scrape

--- models/Article.js
+++ models/Article.js
@@ -8,10 +8,6 @@ var ArticleSchema = new Schema({
       type: String,
       required: true
     },
-    summary: {
-      type: String,
-      required: true
-    },
     link: {
       type: String,
       required: true
--- server.js
+++ server.js
@@ -23,44 +23,61 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/buzzfeedscraper", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/huffpostscraper", { useNewUrlParser: true });
 
 app.get("/scrape", function(req, res) {
-    // First, we grab the body of the html with axios
-    axios.get("http://www.echojs.com/").then(function(response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
+    axios.get("https://www.huffpost.com/").then(function(response) {
       var $ = cheerio.load(response.data);
   
-      // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function(i, element) {
+      $("div.card__headlines").each(function(i, element) {
         // Save an empty result object
         var result = {};
   
-        // Add the text and href of every link, and save them as properties of the result object
         result.title = $(this)
-          .children("a")
+          .find("h3")
           .text();
         result.link = $(this)
           .children("a")
           .attr("href");
-  
-        // Create a new Article using the `result` object built from scraping
+
         db.Article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
       });
+      
+    //   var entry = new Article(result);
+
+    //   entry.save(function(err, res) {
+    //     if (err) {
+    //       console.log(err);
+    //     }
+    //     else {
+    //       console.log(res);
+    //     }
+    //   });
   
       // Send a message to the client
       res.send("Scrape Complete");
+      console.log("Scrape Complete");
     });
 });
 
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+});
 
 
   
