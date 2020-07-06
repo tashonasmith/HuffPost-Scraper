16c198f26792f3cfdd9114e6822d7ac6a97f2bb1
Tashona Smith <tashona.smith@gmail.com>
2020-07-06 00:50:37 -0500

    update port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.port || 3000;
 
 // Initialize Express
 var app = express();
